9c15aedda4e611428c9db01be2e8d81cd65cc75e
sultanf110 <sultanf110@gmail.com>
2025-07-24 20:15:28 +0300

    updated

--- get_position.h
+++ get_position.h
@@ -8,4 +8,14 @@
 #include <unistd.h>
 #include "motors.h"
 
+
+#define MOTOR_N1 0
+#define MOTOR_N2 0
+#define MOTOR_N3 0
+#define MOTOR_N4 0
+
+//the limits of the axis of movement
+#define N_MAX 512
+#define N_MIN 0
+
 int get_position(int steps, int direction);
